6111352e4003769eeef63075e46fe6e6a0f24262
hesimin <hesimin@mftour.cn>
2017-04-01 17:44:34 +0800

    Scheduled 、junit

---
+++ src/main/java/com/github/hesimin/springboot/config/SchedulingConfig.java
@@ -0,0 +1,21 @@
+package com.github.hesimin.springboot.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * @author hesimin 2017-04-01
+ */
+@Configuration
+@EnableScheduling // 启用定时任务
+public class SchedulingConfig {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Scheduled(cron = "0/20 * * * * ?") // 每20秒执行一次
+    public void scheduler() {
+        logger.info(" ====> scheduled ... ");
+    }
+}
